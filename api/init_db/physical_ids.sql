beae501b16f98da818e29477feddf6ca4e166aa1
Javier Morales <morales@itc.nl>
2021-07-07 17:06:44 +0200

    Update physical_ids.sql

--- api/init_db/physical_ids.sql
+++ api/init_db/physical_ids.sql
@@ -1,29 +1,31 @@
 BEGIN;
 
-	ALTER TABLE inspection.spatialunit
+    SET search_path = inspection;
+
+	ALTER TABLE spatialunit
     ADD COLUMN phy_ids character varying;
 
-	UPDATE inspection.spatialunit
+	UPDATE spatialunit
 	SET phy_ids = physical_id;
 
-	UPDATE inspection.spatialunit
+	UPDATE spatialunit
 	SET physical_id = null;
 
-	ALTER TABLE inspection.spatialunit
+	ALTER TABLE spatialunit
 	ALTER COLUMN physical_id TYPE text[] USING physical_id::text[];
 
-	UPDATE inspection.spatialunit
-	SET physical_id =
-	array[split_part(replace(rtrim(replace(phy_ids, E'\n', ' ' )), ' ', ','),',',1),
-		  split_part(replace(rtrim(replace(phy_ids, E'\n', ' ' )), ' ', ','),',',2)];
+	UPDATE spatialunit
+	SET physical_id = array[split_part(replace(rtrim(replace(phy_ids, E'\n', ' ' )), ' ', ','),',',1),
+                            split_part(replace(rtrim(replace(phy_ids, E'\n', ' ' )), ' ', ','),',',2),
+                            split_part(replace(rtrim(replace(phy_ids, E'\n', ' ' )), ' ', ','),',',3)];
 
-	UPDATE inspection.spatialunit
+	UPDATE spatialunit
 	SET physical_id = array_remove(physical_id, '');
-	
-	UPDATE inspection.spatialunit
+
+	UPDATE spatialunit
 	SET physical_id = array_remove(physical_id, null);
 
-	ALTER TABLE inspection.spatialunit
+	ALTER TABLE spatialunit
 	DROP COLUMN phy_ids;
 
 END;
